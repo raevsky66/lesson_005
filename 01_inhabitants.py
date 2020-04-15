6250858637f43c1bc240b549f4ccd5f056c6b3df
raevsky <raevsky66@gmail.com>
2020-04-15 15:42:10 +0500

    solve 01

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -2,7 +2,11 @@
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
+from pprint import pprint
 
-# TODO здесь ваш код
+from room_1 import folks as folks_r1
+from room_2 import folks as folks_r2
 
+print('В комнате room_1 живут:', folks_r1)
+print('В комнате room_2 живут:', folks_r2)
 
