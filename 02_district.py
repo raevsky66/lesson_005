a1e4d1cd2ddf54a6c12c3545d792d52f55620219
raevsky66 <raevsky66@gmail.com>
2020-04-16 16:02:17 +0500

    solve 02

--- 02_district.py
+++ 02_district.py
@@ -4,9 +4,34 @@
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
+from pprint import pprint
 
-# TODO здесь ваш код
+from district.central_street.house1.room1 import folks as folks_cs_h1_r1
+from district.central_street.house1.room2 import folks as folks_cs_h1_r2
+from district.central_street.house2.room1 import folks as folks_cs_h2_r1
+from district.central_street.house2.room2 import folks as folks_cs_h2_r2
 
+from district.soviet_street.house1.room1 import folks as folks_ss_h1_r1
+from district.soviet_street.house1.room2 import folks as folks_ss_h1_r2
+from district.soviet_street.house2.room1 import folks as folks_ss_h2_r1
+from district.soviet_street.house2.room2 import folks as folks_ss_h2_r2
 
+def get_tenants(list_tenants):
+    tenants = ','
+    # for tenant in list_tenants:
+    #     tenants = tenants.join(tenant)
+    return tenants.join(list_tenants)
 
+str = get_tenants(folks_ss_h1_r1)
+str = str + ','+ get_tenants(folks_ss_h1_r2)
+str = str + ','+ get_tenants(folks_ss_h2_r1)
+str = str + ','+ get_tenants(folks_ss_h2_r2)
+
+str = str + ','+ get_tenants(folks_cs_h1_r1)
+str = str + ','+ get_tenants(folks_cs_h1_r2)
+str = str + ','+ get_tenants(folks_cs_h2_r1)
+str = str + ','+ get_tenants(folks_cs_h2_r2)
+
+# str = str.join(get_tenants(folks_cs_h1_r1))
+print(str)
 
